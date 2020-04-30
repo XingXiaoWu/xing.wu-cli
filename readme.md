62f21d0050cb7a711c69836120a78e7b296681af
无星 <32910694@qq.com>
2020-04-30 22:45:35 +0800

    重构操作

--- readme.md
+++ readme.md
@@ -1,2 +1,30 @@
+查看版本
 
-update-notifier :cli更新后续可以加上
+```
+wxplatform-cli -V
+szyx-wxplatform --version
+```
+
+### 三方库介绍
+
+```
+<!-- 终端样式库 -->
+"chalk": "^3.0.0",
+<!-- 命令行交互 -->
+"commander": "^5.0.0",
+<!-- 从git拉模板 -->
+"download-git-repo": "^3.0.2",
+<!-- fs操作拓展 -->
+"fs-extra": "^9.0.0",
+<!-- 模板引擎 -->
+"handlebars": "^4.7.3",
+<!-- 命令行交互 -->
+"inquirer": "^7.1.0",
+<!-- 日志输出 -->
+"log-symbols": "^3.0.0",
+<!-- 加载效果，图标 -->
+"ora": "^4.0.3",
+<!-- 检验npm名字取的对不对 -->
+"validate-npm-package-name": "^3.0.0"
+update-notifier :cli 更新后续可以加上
+```
--- src/index.js
+++ src/index.js
@@ -1,94 +1,28 @@
 #! /usr/bin/env node
+//--inspect-brk
+const create = require('../lib/init')
 // 用来进行命令行交互的
-const program = require("commander");
-// 读取处理文件
-const fs = require("fs");
-// 用于下载远程template的
-const download = require("download-git-repo");
-// 添加命令行输出背景色
-const chalk = require("chalk");
+const { program } = require('commander')
 
-const handlebars = require("handlebars");
-// 	命令行中可以使用对话或者选项了
-const inquirer = require("inquirer");
-//  动态图标
-const ora = require("ora");
-// 花式打log
-const symbols = require("log-symbols");
 // 读取packgaejson
-const packageConfig = require("../package.json");
+const packageConfig = require('../package.json')
+// 这样输出-V或--version就能看到版本号了
+program.version(packageConfig.version)
 
 program
-  .version(packageConfig.version)
-  .description("欢迎使用无星的cli进行初始化")
-  .command("init <name>")
-  .action(name => {
-    // 这里拿到name了，判断当前文件夹下，是否有同名文件夹
-    if (!fs.existsSync(name)) {
-      // 不存在，开始创建
-      // 需要交互的内容
+  // 命令
+  .command('init <name>')
+  .alias('i')
+  // -h的时候命令描述
+  .description('新建项目的命令')
+  .action((name, cmd) => {
+    console.log('开始新建项目，项目名' + name)
+    create(name)
+  })
 
-      const promptList = [
-        // 具体交互内容
-        {
-          name: "type",
-          message: "vue:1;react:2"
-        },
-        {
-          name: "description",
-          message: "请输入项目描述"
-        },
-        {
-          name: "author",
-          message: "请输入作者名"
-        }
-      ];
-      inquirer.prompt(promptList).then(answers => {
-        // 判断类型，下载不同类型模板
-        const { type, description, author } = answers;
-        let url = "";
-        if (type === "1") {
-          console.log("正在拉取vue项目模板");
-          url = "https://gitee.com:wuxinggg/vue-template#master";
-        } else {
-          console.log("正在拉取react项目模板");
-          url = "https://gitee.com:wuxinggg/react-template#master";
-        }
-        const spinner = ora("download...");
-        spinner.start();
-        download(url, name, { clone: true }, err => {
-          // 判断是否存在错误
-          if (err) {
-            // 下载失败，展示原因
-            spinner.fail();
-            console.log(chalk.red(err));
-          } else {
-            // 下载成功
-            spinner.succeed();
-            const fileName = name + "/package.json";
-            const meta = {
-              name,
-              description,
-              author
-            };
-            // 判断路径
-            if (fs.existsSync(fileName)) {
-              // 写入数据
-              const content = fs.readFileSync(fileName).toString();
-              const result = handlebars.compile(content)(meta);
-              fs.writeFileSync(fileName, result);
-            }
-            console.log(chalk.green("success"));
-            console.log(chalk.magenta(`${name}创建成功, 执行下面命令启动项目`));
-            console.log(chalk.magenta(` cd ${name}`));
-            console.log(chalk.magenta(" npm install"));
-            console.log(chalk.magenta(" npm start"));
-          }
-        });
-      });
-    } else {
-      // 存在，直接报错
-      console.log(symbols.error, chalk.red("项目已存在"));
-    }
-  });
-program.parse(process.argv);
+program.on('--help', () => {
+  console.log('新需求？可反馈给329106954@qq.com')
+})
+
+// 必须放到最后一行用于解析
+program.parse(process.argv)
