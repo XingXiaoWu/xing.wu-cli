51d71eaee333dc6a532c96b09b1f6242b05fb340
无星 <32910694@qq.com>
2020-09-06 17:29:44 +0800

    重写

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-#! /usr/bin/env node
-//--inspect-brk
+#!/usr/bin/env node
+// --inspect-brk
 const create = require('../lib/init')
+const onList = require('../lib/onList')
+const onAdd = require('../lib/onAdd')
+const onDel = require('../lib/onDel')
 // 用来进行命令行交互的
 const { program } = require('commander')
 
@@ -9,19 +12,36 @@ const packageConfig = require('../package.json')
 // 这样输出-V或--version就能看到版本号了
 program.version(packageConfig.version)
 
+// init
 program
-  // 命令
-  .command('init <name>')
-  .alias('i')
-  // -h的时候命令描述
-  .description('新建项目的命令')
-  .action((name, cmd) => {
-    console.log('开始新建项目，项目名' + name)
-    create(name)
-  })
+    .command('init <name>')
+    .alias('i')
+    .description('新建项目的命令')
+    .action((name, cmd) => {
+        console.log('开始新建项目，项目名' + name)
+        create(name)
+    })
+
+// 查看模板
+program
+    .command('ls')
+    .description('查看当前所有模板')
+    .action(onList)
+
+// 添加模板
+program
+    .command('add <name> <url>')
+    .description('本地添加模板, 域名:所有者/项目名称#分支 例如 https://gitea.51trust.com:front/vueDemo#master')
+    .action(onAdd);
+
+// 移除模板
+program
+    .command('del <name>')
+    .description('移除本地模板')
+    .action(onDel);
 
 program.on('--help', () => {
-  console.log('新需求？可反馈给329106954@qq.com')
+    console.log('新需求？可反馈给329106954@qq.com')
 })
 
 // 必须放到最后一行用于解析
---
+++ utils/index.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const fs = require('fs-extra')
+const chalk = require('chalk')
+const symbols = require('log-symbols')
+const _ = require('lodash')
+const customregist = require('../json/customregist.json')
+const registries = require('../json/registries.json')
+/**
+ * 当前cli所在路径
+ */
+const cliPath = path.resolve(".")
+
+/**
+ * 
+ * @param {*} filepath file路径,需要带/,默认拼接cli所在路径cliPath
+ * @param {*} object 需要写入的对象
+ */
+function wirteSync (filepath, jsonObject) {
+    fs.writeFileSync(cliPath + filepath, JSON.stringify(jsonObject, null, '\t'))
+}
+/**
+ * 
+ * @param {*} message 成功打印
+ */
+function successConsole (message) {
+    console.log(chalk.green(message))
+}
+
+/**
+ * 
+ * @param {*} message 错误打印
+ */
+function errorConsole (message) {
+    console.log(symbols.error, chalk.red(message))
+}
+/**
+ * 
+ * @param {*} url git地址转成可下载地址
+ */
+function gitTogitdown (url) {
+    // 最后的地址格式
+    // 域名:所有者/项目名称#分支
+    // 例如https://gitea.51trust.com:front/vueDemo#master
+    // "vue":"https://gitea.51trust.com:front/vueDemo#master",
+    // "vue2":"https://gitea.51trust.com/front/vueDemo.git"
+    // let tmpUrl = _.replace(url, ".git", "#master")
+    // let tmpArray = _.split(tmpUrl, '/');
+    // let result = tmpArray[0] + "//" + tmpArray[2] + ":" + tmpArray[3] + "/" + tmpArray[4]
+    return url
+}
+
+/**
+ * 获取所有template
+ */
+function getAllTemplates(){
+    let all = {
+        ...registries,
+        ...customregist,
+    }
+    return all
+}
+
+module.exports = {
+    cliPath,
+    wirteSync,
+    successConsole,
+    errorConsole,
+    gitTogitdown,
+    getAllTemplates
+}
